9b089391dea6d0c8493779a18efce10d71693cfd
Matheus Matias <matheus.ferreira@gigalink.net.br>
2024-10-23 12:39:54 -0300

    Correcao para boletos com nosso numero no formato anterior

--- lib/brcobranca/boleto/sicoob.rb
+++ lib/brcobranca/boleto/sicoob.rb
@@ -5,7 +5,8 @@ module Brcobranca
     class Sicoob < Base # Sicoob (Bancoob)
       validates_length_of :agencia, maximum: 4, message: 'deve ser menor ou igual a 4 dígitos.'
       validates_length_of :conta_corrente, maximum: 8, message: 'deve ser menor ou igual a 8 dígitos.'
-      validates_length_of :nosso_numero, maximum: 8, message: 'deve ser menor ou igual a 8 dígitos.'
+      validates_length_of :nosso_numero, maximum: 7, message: 'deve ser menor ou igual a 7 dígitos.', if: -> { valida_digito_verificador }
+      validates_length_of :nosso_numero, maximum: 8, message: 'deve ser menor ou igual a 8 dígitos.', if: -> { !valida_digito_verificador }
       validates_length_of :convenio, maximum: 7, message: 'deve ser menor ou igual a 7 dígitos.'
       validates_length_of :variacao, maximum: 2, message: 'deve ser menor ou igual a 2 dígitos.'
       validates_length_of :quantidade, maximum: 3, message: 'deve ser menor ou igual a 3 dígitos.'
@@ -47,7 +48,8 @@ module Brcobranca
       #
       # @return [String] 7 caracteres numéricos.
       def nosso_numero=(valor)
-        @nosso_numero = valor.to_s.rjust(8, '0') if valor
+        @nosso_numero = valor.to_s.rjust(7, '0') if valor && valida_digito_verificador
+        @nosso_numero = valor.to_s.rjust(8, '0') if valor && !valida_digito_verificador
       end
 
       # Quantidade
